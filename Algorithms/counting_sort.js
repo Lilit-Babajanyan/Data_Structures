8a9c5af67433765abb12e914db2b46cac560ecd1
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-07-01 02:14:31 +0400

    Counting Sort

--- Algorithms/counting_sort.js
+++ Algorithms/counting_sort.js
@@ -1,8 +1,6 @@
 function counting_sort(arr) {
-    
     let n = arr.length;
     let max = arr[0];
-    let count = new Array(max + 1).fill(0);
 
     for (let i = 1; i < n; i++) {
         if (arr[i] > max) {
@@ -10,12 +8,13 @@ function counting_sort(arr) {
         }
     }
 
+    let count = new Array(max + 1).fill(0);
+
     for (let i = 0; i < n; i++) {
         count[arr[i]]++;
     }
 
     let k = 0;
-
     for (let i = 0; i <= max; i++) {
         while (count[i] > 0) {
             arr[k] = i;
